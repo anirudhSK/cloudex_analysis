28314e1a858cb03c2492796455fd41af38de8978
Anirudh Sivaraman <sk.anirudh@gmail.com>
2020-11-09 12:49:26 -0500

    Get qdelays binned in 1 second increments.
    A bit slower than finding min and max times (112 s vs. 126 s on 4GB dataset).

--- analyze.rs
+++ analyze.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 use std::cmp;
 use std::i64;
 use std::str::FromStr;
+const NUM_SECONDS : usize      = 10000;
 
 fn main() {
     let mut record_count     = 0;
@@ -17,7 +18,9 @@ fn main() {
     let mut deq_ts_index     = usize::MAX;
     let mut qdelay_ts_index  = usize::MAX; 
     let mut column_name_to_index_map = HashMap::new();
-
+    let mut qdelaysum_vector  : Vec::<i64> = vec![0; NUM_SECONDS]; // one entry for each second.
+    let mut qdelaycount_vector: Vec::<i64> = vec![0; NUM_SECONDS]; // one entry for each second.
+    let base_time = i64::from_str(&env::args().nth(2).unwrap()).unwrap();
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines(env::args().nth(1).unwrap()) {
         // Consumes the iterator, returns an (Optional) String
@@ -44,11 +47,14 @@ fn main() {
                     record_count += 1;
                     let tmp = line.unwrap(); 
                     let records = tmp.split(',').collect::<Vec::<&str>>();
+                    let now  = i64::from_str(records[ts_index]).unwrap();
                     let orig_qdelay = (f64::from_str(records[qdelay_ts_index]).unwrap() * 1000.0).round() as i64;
-                    start_time = cmp::min(i64::from_str(records[ts_index]).unwrap(), start_time);
-                    end_time   = cmp::max(i64::from_str(records[ts_index]).unwrap(), end_time);
+                    start_time = cmp::min(now, start_time);
+                    end_time   = cmp::max(now, end_time);
                     let qdelay     = i64::from_str(records[deq_ts_index]).unwrap() -
                                      i64::from_str(records[enq_ts_index]).unwrap();
+                    qdelaysum_vector[((now-base_time)/1000000) as usize] += qdelay;
+                    qdelaycount_vector[((now-base_time)/1000000) as usize] += 1;
                     assert!(orig_qdelay == qdelay);
                     if record_count % 10000 == 0 {
                         println!("Done with {} records", record_count);
@@ -58,6 +64,15 @@ fn main() {
         }
     }
     println!("Total records: {}, start_time = {}, end_time = {}", record_count, start_time, end_time);
+    let mut avg_qdelay = Vec::new();
+    for i in 0..NUM_SECONDS {
+        if qdelaycount_vector[i] == 0 {
+            avg_qdelay.push(-1.0000);
+        } else {
+            avg_qdelay.push(qdelaysum_vector[i] as f64/qdelaycount_vector[i] as f64);
+        }
+    }
+    println!("Queuing delay: {:#?}", avg_qdelay);
 }
 
 // The output is wrapped in a Result to allow matching on errors
