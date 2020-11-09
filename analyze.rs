fa45a568b63ce7c4af0a46801cabb5c0ef1d65e1
Anirudh Sivaraman <sk.anirudh@gmail.com>
2020-11-09 00:29:13 -0500

    Compute start and end time. 10x slower than counting records.

--- analyze.rs
+++ analyze.rs
@@ -3,22 +3,26 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::env;
 use std::collections::HashMap;
+use std::cmp;
+use std::i64;
+use std::str::FromStr;
 
 fn main() {
     let mut record_count     = 0;
     let mut first_line_done  = false;
+    let mut start_time       = i64::MAX;
+    let mut end_time         = 0;
+    let mut ts_index         = usize::MAX;
     let mut column_name_to_index_map = HashMap::new();
 
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines(env::args().nth(1).unwrap()) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
-            if line.is_ok() {
-                record_count += 1;
-            }
             if ! first_line_done {
-                // Split unwrap from split to avoid temporary value borrowed while dropped
+                // Split unwrap from split to avoid "temporary value borrowed while dropped" error
                 // https://users.rust-lang.org/t/can-not-understand-temporary-value-dropped-while-borrowed/23279/7
+                // https://stackoverflow.com/a/26080489
                 let tmp = line.unwrap();
                 let column_names = tmp.split(',').collect::<Vec::<&str>>();
                 let mut i = 0;
@@ -28,10 +32,22 @@ fn main() {
                 }
                 first_line_done = true;
                 println!("Print column_name_to_index_map: {:#?}", column_name_to_index_map);
+                ts_index = column_name_to_index_map["Genesis-Timestamp"];
+            } else {
+                if line.is_ok() {
+                    record_count += 1;
+                    let tmp = line.unwrap(); 
+                    let records = tmp.split(',').collect::<Vec::<&str>>();
+                    start_time = cmp::min(i64::from_str(records[ts_index]).unwrap(), start_time);
+                    end_time   = cmp::max(i64::from_str(records[ts_index]).unwrap(), end_time);
+                    if record_count % 1000 == 0 {
+                        println!("Done with {} records", record_count);
+                    }
+                }
             }
         }
     }
-    println!("Total records: {}", record_count);
+    println!("Total records: {}, start_time = {}, end_time = {}", record_count, start_time, end_time);
 }
 
 // The output is wrapped in a Result to allow matching on errors
